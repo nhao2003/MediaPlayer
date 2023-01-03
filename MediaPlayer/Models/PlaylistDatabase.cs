8aff40b5e871157cf635a7b7fe873a11b8f19ba8
Surgeous <tuanbao2311@gmail.com>
2023-01-03 09:33:02 +0700

    Tạo class xử lý database (đã xử lý được phần tạo + insert)

---
+++ MediaPlayer/Models/PlaylistDatabase.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SQLite;
+using System.Windows.Forms;
+namespace MediaPlayer.Models
+{
+    internal class PlaylistDatabase
+    {
+        static string playlistDatabaseFilePath = "Data Source=" + Environment.CurrentDirectory + "\\Playlist.db";
+        static SQLiteConnection playlistDatabaseConnection = new SQLiteConnection(playlistDatabaseFilePath);
+
+        public void InitializeTables()
+        {
+            string createPlaylistTable = "CREATE TABLE IF NOT EXISTS Playlist(" +
+                "PlaylistID VARCHAR(4000) PRIMARY KEY NOT NULL," +
+                "PlaylistName VARCHAR(4000) NOT NULL," +
+                "PlaylistThumbnailPath VARCHAR(4000)," +
+                "PlaylistDateOfCreation SMALLDATETIME " +
+                ")";
+            try
+            {
+                CreateTable(createPlaylistTable);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error Creating Playlist Table");
+            }
+
+            string createPlaylistMediasTable = "CREATE TABLE IF NOT EXISTS PlaylistMedias(" +
+                "MediaPath VARCHAR(4000) PRIMARY KEY NOT NULL," +
+                "PlaylistID VARCHAR(4000)," +
+                "FOREIGN KEY (PlaylistID) REFERENCES Playlist(PlaylistID)" +
+                ")";
+            try
+            {
+                CreateTable(createPlaylistMediasTable);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Creating PlaylistMedias Table");
+            }
+        }
+
+
+        public void CreateTable(string tableCreationString)
+        {
+            playlistDatabaseConnection.Open();
+
+            var sqlCommand = new SQLiteCommand(tableCreationString, playlistDatabaseConnection);
+            RunSqlCommand(sqlCommand);
+
+            playlistDatabaseConnection.Close();
+        }
+
+        private void RunSqlCommand(SQLiteCommand sqlCommand)
+        {
+            try
+            {
+                int result = sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void InsertPlaylist(Playlist insertPlaylist)
+        {
+            playlistDatabaseConnection.Open();
+
+            string insertQuery = "INSERT INTO Playlist "
+                + "VALUES(@playlistID, @playlistName, @playlistThumbnailPath, @playlistDateOfCreation)";
+            SQLiteCommand sqlCommand = new SQLiteCommand(insertQuery, playlistDatabaseConnection);
+
+            AddValueIntoPlaylistinSQLCommand(insertPlaylist, sqlCommand);
+            RunSqlCommand(sqlCommand);
+
+            playlistDatabaseConnection.Close();
+        }
+
+        public void InsertMediaIntoPlaylistMedias(Playlist playlistContainsMedia, Media insertMedia)
+        {
+            playlistDatabaseConnection.Open();
+
+            string insertQuery = "INSERT INTO PlaylistMedias "
+                + "VALUES(@mediaPath, @playlistID)";
+            SQLiteCommand sqlCommand = new SQLiteCommand(insertQuery, playlistDatabaseConnection);
+
+            AddValueIntoPlaylistMediasinSQLCommand(playlistContainsMedia, insertMedia, sqlCommand);
+            RunSqlCommand(sqlCommand);
+
+            playlistDatabaseConnection.Close();
+        }
+
+
+        public void TestInsertOperation()
+        {
+            Playlist testPlaylist = new Playlist("TestInsert", @"C:\Users\tuanb\Downloads\Pictures\DOkiRkcWkAAojb-.jfif");
+            Media testMedia = new Media(@"C:\Users\tuanb\Downloads\Music\Dangerous Nightcore - Deamn.mp3");
+
+            try
+            {
+                this.InsertPlaylist(testPlaylist);
+                this.InsertMediaIntoPlaylistMedias(testPlaylist, testMedia);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void AddValueIntoPlaylistMediasinSQLCommand(Playlist playlistContainsMedia, Media insertMedia, SQLiteCommand sqlCommand)
+        {
+            sqlCommand.Parameters.AddWithValue("@mediaPath", insertMedia.FilePath);
+            sqlCommand.Parameters.AddWithValue("@playlistID", playlistContainsMedia.PlayListID);
+        }
+
+        private void AddValueIntoPlaylistinSQLCommand(Playlist insertPlaylist, SQLiteCommand sqlCommand)
+        {
+            sqlCommand.Parameters.AddWithValue("@playlistID", insertPlaylist.PlayListID);
+            sqlCommand.Parameters.AddWithValue("@playlistName", insertPlaylist.PlayListName);
+            sqlCommand.Parameters.AddWithValue("@playlistThumbnailPath", insertPlaylist.BackroundImageFileName);
+            sqlCommand.Parameters.AddWithValue("@playlistDateOfCreation", insertPlaylist.DateCreated);
+        }
+    }
+}
